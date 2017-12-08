7e41fd8fcee944a5d7b01b4a280764a9543ed31e
klimas7 <klimas7@gmail.com>
2017-12-08 16:48:32 +0100

    Max hour

--- src/main/java/pl/klimas7/learn/hour/Hour.java
+++ src/main/java/pl/klimas7/learn/hour/Hour.java
@@ -1,9 +1,58 @@
 package pl.klimas7.learn.hour;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 public class Hour {
-    public LocalTime getMaxHour(Integer ... argss) throws IllegalArgumentException {
+    public LocalTime getMaxHour(Integer... argss) throws IllegalArgumentException {
+        validate(argss);
+
+        List<Integer> data = new ArrayList();
+        data.addAll(Arrays.asList(argss));
+
+        Integer majorHour = getMajorHour(data);
+        data.remove(majorHour);
+
+        Integer minorHour = getMinorHour(data, majorHour);
+        data.remove(minorHour);
+
+        Integer majorMinute = getMajorMinute(data);
+        data.remove(majorMinute);
+        Integer minorMinute = data.get(0);
+
+        int hour = majorHour * 10 + minorHour;
+        int minute = majorMinute * 10 + minorMinute;
+        return LocalTime.of(hour, minute);
+    }
+
+    private Integer getMajorHour(List<Integer> data) {
+        return data.stream()
+                .sorted(Comparator.reverseOrder())
+                .filter(i -> i <= 2)
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    private Integer getMinorHour(List<Integer> data, Integer majorHour) {
+        return data.stream()
+                .sorted(Comparator.reverseOrder())
+                .filter(i -> ((majorHour == 2 && i <= 3) || (majorHour != 2)))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    private Integer getMajorMinute(List<Integer> data) {
+        return data.stream()
+                .sorted(Comparator.reverseOrder())
+                .filter(i -> i <= 5)
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    private void validate(Integer[] argss) {
         if (argss == null) {
             throw new IllegalArgumentException("Input parameter can't be null");
         }
@@ -11,9 +60,5 @@ public class Hour {
         if (argss.length != 4) {
             throw new IllegalArgumentException("Must specified 4 parameters");
         }
-
-        int hour = 0;
-        int minute = 0;
-        return LocalTime.of(hour, minute);
     }
 }
--- src/test/java/pl/klimas7/learn/hour/HourTest.java
+++ src/test/java/pl/klimas7/learn/hour/HourTest.java
@@ -1,13 +1,65 @@
 package pl.klimas7.learn.hour;
 
 
+import java.time.LocalTime;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 public class HourTest {
+
     @Test(expected = IllegalArgumentException.class)
     public void testNullArguments() {
         Hour hour = new Hour();
 
         hour.getMaxHour(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testNotEnoughArguments() {
+        Hour hour = new Hour();
+
+        hour.getMaxHour(0, 2);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testWrongMajorHour() {
+        Hour hour = new Hour();
+
+        hour.getMaxHour(3, 4, 5, 6);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testWrongMajorMinut() {
+        Hour hour = new Hour();
+
+        hour.getMaxHour(2, 3, 6, 6);
+    }
+
+    @Test
+    public void testGetMaxHourA() {
+        Hour hour = new Hour();
+
+        LocalTime maxHour = hour.getMaxHour(0, 0, 2, 3);
+
+        Assert.assertEquals(LocalTime.of(23, 00), maxHour);
+    }
+
+    @Test
+    public void testGetMaxHourB() {
+        Hour hour = new Hour();
+
+        LocalTime maxHour = hour.getMaxHour(9, 1, 5, 9);
+
+        Assert.assertEquals(LocalTime.of(19, 59), maxHour);
+    }
+
+    @Test
+    public void testGetMaxHourC() {
+        Hour hour = new Hour();
+
+        LocalTime maxHour = hour.getMaxHour(0, 0, 0, 0);
+
+        Assert.assertEquals(LocalTime.of(0, 0), maxHour);
     }
 }
