d3cb4e485b552e8fced028da8d6f8fcc89290665
klimas7 <klimas7@gmail.com>
2019-06-05 23:20:58 +0200

    devoxx219 part1

---
+++ src/main/java/pl/sii/puzzles/devoxx2019/SiiLocations.java
@@ -0,0 +1,35 @@
+package pl.sii.puzzles.devoxx2019;
+
+import lombok.AllArgsConstructor;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.Comparator.comparing;
+
+public class SiiLocations {
+    public static void main(String[] args) {
+        List<Location> locations = Arrays.asList(
+                new Location("Warsaw", 847),
+                new Location("Gdansk", 910),
+                new Location("Wroclaw", 478),
+                new Location("Cracow", 504));
+
+        locations.add(new Location("Rzeszow", 50));
+        locations.add(new Location("Bydgoszcz", 36));
+
+        Stream<Location> locationStream = locations.stream();
+
+        System.out.println("Sii locations: " + locationStream.count());
+        locationStream.max(comparing(l -> l.workers)).ifPresent(l -> System.out.println(l.name));
+    }
+
+    @AllArgsConstructor
+    static class Location {
+        String name;
+        Integer workers;
+    }
+}
+
+
